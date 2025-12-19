af46054635755461f116ee59701ab8082b768261
aprendeineamx-max <aprendeinea.mx@gmail.com>
2025-12-18 19:50:04 -0700

    Pruebas...

---
+++ TEST_WORKSPACE/permission_test_2.js
@@ -0,0 +1,11 @@
+// TEST PERMISSION LISTENER - Archivo 2
+// Segunda prueba de autorización
+
+console.log("Testing permission listener - File 2");
+
+const data = {
+    test: "permission_tracking",
+    counter_should_increment: true
+};
+
+module.exports = data;
